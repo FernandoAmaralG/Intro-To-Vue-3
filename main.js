efa2910a4102f3ce2f012587de5dd76a230113a9
FernandoAmaralG <fernandoamaral@unipam.edu.br>
2023-11-18 18:14:56 -0300

    Event Handling

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            image: './assets/images/socks_blue.jpg',
             url: 'https://www.vuemastery.com/',
             description: 'A warm fuzzy pair of socks.',
             inventory: 100,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
             sizes: ['S', 'M', 'L', 'XL']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if(this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
