2e372ae46fc95ec71e7bad9333aaff893edd6699
FernandoAmaralG <fernandoamaral@unipam.edu.br>
2023-11-18 13:32:57 -0300

    Attibute Binding

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/',
             description: 'A warm fuzzy pair of socks.'
         }
     }
